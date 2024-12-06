b97891fd2a7cfc12b9f51a7e556987f42bcf7d06
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-12-06 21:14:58 +0530

    add styles

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-red-500">Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
